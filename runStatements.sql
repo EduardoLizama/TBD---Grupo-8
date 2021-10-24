724b0c6b88c3e1cbab77de0fdfe15c8968f37ad4
Camila-Garcia <64158104+Camila-Garcia@users.noreply.github.com>
2021-10-24 19:25:47 -0300

    Sentencias 2 y 3

--- runStatements.sql
+++ runStatements.sql
@@ -1,6 +1,16 @@
 select * from pago_gg_depto
-where morosidad = false and fecha_emision > Current_Date - interval '3 months'
+where morosidad = false and fecha_emision > Current_Date - interval '3 months';
 
 select d.id_edificio, d.piso, MAX(d.habitantes) AS habitantes
 From depto as d
 group by d.id_edificio, d.piso;
+
+SELECT e.id_edificio,EXTRACT(MONTH FROM pgd.fecha_emision) AS mes,SUM(gc.monto) as Total_gasto_comun
+FROM edificio AS e, pago_gg_depto AS pgd, gasto_comun AS gc
+WHERE (pgd.id_gasto_comun=gc.id_gasto_comun AND gc.id_edificio=e.id_edificio) AND pgd.morosidad=False
+GROUP BY mes, e.id_edificio;
+
+SELECT EXTRACT(MONTH FROM pge.fecha_emision) AS mes,e.id_edificio,SUM(ge.monto) as Total_gasto_edificio
+FROM edificio AS e, pago_gasto_edificio AS pge, gasto_edificio as ge
+WHERE e.id_edificio=pge.id_edificio AND pge.id_gasto_edificio=ge.id_gasto_edificio AND pge.morosidad=False
+GROUP BY mes, e.id_edificio;
