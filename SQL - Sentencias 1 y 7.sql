255aca5affa8190a0e325586c5cfb41925bdfa6e
EduardoLizama <74266905+EduardoLizama@users.noreply.github.com>
2021-10-24 17:45:51 -0300

    Add files via upload

---
+++ SQL - Sentencias 1 y 7.sql
@@ -0,0 +1,6 @@
+select * from pago_gg_depto
+where morosidad = false and fecha_emision < Current_Date - interval '3 months'
+
+select d.id_edificio, d.piso, MAX(d.habitantes)
+From depto as d
+group by d.id_edificio, d.piso;
